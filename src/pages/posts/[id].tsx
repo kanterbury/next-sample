d8c80f73bbcd237552d329dd5e538641dad9a737
kanterbury <ksuzuki.db@gmail.com>
2023-04-29 17:50:52 +0900

    up to isr

---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,68 @@
+import { NextPage, GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+
+interface PostProps {
+  id: string;
+}
+
+const Post: NextPage<PostProps> = ({ id }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>このページは静的サイト生成によってビルド時に生成されたページです</p>
+        <p>{`/posts/${id}に対応するページです`}</p>
+      </main>
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = [
+    {
+      params: {
+        id: "1",
+      },
+    },
+    {
+      params: {
+        id: "2",
+      },
+    },
+    {
+      params: {
+        id: "3",
+      },
+    },
+  ];
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+interface PostParams extends ParsedUrlQuery {
+  id: string;
+}
+
+export const getStaticProps: GetStaticProps<PostProps, PostParams> = async (
+  context
+) => {
+  return {
+    props: {
+      id: context.params!.id,
+    },
+  };
+};
+
+export default Post;
